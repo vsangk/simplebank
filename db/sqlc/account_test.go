16cb26ae158a981978cba33831d2e77bd7939164
Vince Sangkhae <vsangk@gmail.com>
2023-06-11 11:22:41 -0700

    Add random test generators

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -5,13 +5,14 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	"github.com/vsangk/simplebank/util"
 )
 
 func TestCreateAccount(t *testing.T) {
 	arg := CreateAccountParams{
-		Owner:    "tom",
-		Balance:  100,
-		Currency: "USD",
+		Owner:    util.RandomOwner(),
+		Balance:  util.RandomBalance(),
+		Currency: util.RandomCurrency(),
 	}
 
 	account, err := testQueries.CreateAccount(context.Background(), arg)
